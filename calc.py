dbcba0c95418d9716efc74e45d336d8b94f3a119
snsepton <sonja.septon@utah.edu>
2021-09-28 22:00:23 -0600

    UnitTesting

---
+++ calc.py
@@ -0,0 +1,12 @@
+
+def add(x,y):
+    return (x+y)
+def sub(x,y):
+    return (x-y)
+def multiply(x, y):
+    return (x*y)
+def divide(x,y):
+    if y == 0:
+        raise ValueError('Can not divide by zero!')
+    return (x/y)
+
